deb8a5fa1e992175dc98edfdc74956f765ee9476
ASAD ALAM <112319884+wanderlust026@users.noreply.github.com>
2023-09-05 21:55:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -119,3 +119,18 @@ scrollBottom.forEach((e1)=>observer.observe(e1));
 
 const scrollTop = document.querySelectorAll(".scroll-top");
 scrollTop.forEach((e1)=>observer.observe(e1));
+
+function sendEmail(){
+    Email.send({
+        SecureToken: "366d1615-360e-4a74-8169-fddba87efeb2",
+        To : 'wanderlustinfo026@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New Contact Form Enquary",
+        Body : "Name : " + document.getElementById("name").value
+               +"<br>Email : "+document.getElementById("email").value
+               +"<br>Phone No. : "+document.getElementById("phone").value
+               +"<br>Message : "+document.getElementById("message").value
+    }).then(
+      message => alert("Message sent Sucessfully")
+    );
+}
